69deed9ba3643f701b9bcc7d013272bd5ac01a1d
smiltenberger <73602064+smiltenberger@users.noreply.github.com>
2022-10-19 22:59:08 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import HomeScreen from './screens/HomeScreen';
+import AddFriend from './screens/AddFriend';
+import AddClass from './screens/AddClass';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer> 
+    <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen 
+        name="HomeScreen" 
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          headerTitle: "FitnessFinder",
+          headerTitleStyle: {
+            fontSize: 30,
+            fontWeight: '400',
+            fontFamily: 'Arial',
+            color: '#489fb5'
+          },
+          headerStyle: {
+            height: 96
+
+          },
+          headerRight: () => (
+            <Icon name="user-plus" size={28} color="#303030" onPress={() => navigation.navigate('AddFriend')} />),
+          headerLeft: () => (
+            <Icon name="calendar-plus" size={30} color="#303030" onPress={() => navigation.navigate('AddClass')} />) 
+        })} />
+        <Stack.Screen 
+        name="AddClass" 
+        component={AddClass} 
+        options={{
+          headerBackTitle: "Back",
+          headerTitle: "Add a class",
+          headerTitleStyle: {
+            fontSize: 20,
+            fontWeight: '400',
+            fontFamily: 'Arial'
+          }
+          }}
+        />
+        <Stack.Screen 
+        name="AddFriend" 
+        component={AddFriend}
+        options={{
+          headerBackTitle: "Back",
+          headerTitle: "Add a friend",
+          headerTitleStyle: {
+            fontSize: 20,
+            fontWeight: '400',
+            fontFamily: 'Arial'
+          }
+          }}  />
+    </Stack.Navigator>
+    
+    </NavigationContainer>
+
+    );
 }
 
 const styles = StyleSheet.create({
